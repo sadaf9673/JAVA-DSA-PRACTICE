6c52aa63423aab9c29fb63ce9fac2215505101fc
sadaf9673 <sadafshaikh.in@gmail.com>
2026-06-30 19:24:47 +0530

    Improve code readability

--- RotateKtimes.java
+++ RotateKtimes.java
@@ -4,15 +4,19 @@ public class RotateKtimes{
     public static void main(String[] args)
 {
     Scanner sc = new Scanner(System.in);
-
+     System.out.print("Enter the size of an array:");
     int size = sc.nextInt();
     int[] arr = new int[size];
 
+    System.out.print("Enter the elements of an array:");
     for(int i=0; i<size; i++){
         arr[i] = sc.nextInt();
     }
+
+    System.out.print("Enter the value of K:");
     int k = sc.nextInt();
 
+    // Handle the case k>size
     k = k%size;
     for(int p=0; p<k; p++){
         int temp = arr[size-1];
